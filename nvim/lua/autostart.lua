b410690b8ccfd7735946601f8d039c22bd7c69d7
qui <qui@vern.cc>
2024-08-31 20:14:34 +0300

    Update nvim

--- nvim/lua/autostart.lua
+++ nvim/lua/autostart.lua
@@ -1,8 +1,8 @@
 vim.api.nvim_create_autocmd("VimEnter", {
   pattern = "*",
   callback = function()
-    if require('nvim-tree') then
-      vim.cmd('NvimTreeToggle')
-    end
+    --if require('nvim-tree') then
+      --vim.cmd('NvimTreeToggle')
+    --end
   end
 })
